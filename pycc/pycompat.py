d85ac30f5f6b7eacb98f0b17d7ad6f76fa146e6a
Kevin Conway <kevinjacobconway@gmail.com>
2014-10-26 13:04:28 -0500

    Add additional version info to pycompat
    Signed-off-by: Kevin Conway <kevinjacobconway@gmail.com>

--- pycc/pycompat.py
+++ pycc/pycompat.py
@@ -6,9 +6,16 @@ from __future__ import unicode_literals
 
 import sys
 
-
-PY2 = sys.version_info.major == 2
+VERSION = sys.version_info
+PY2 = VERSION.major == 2
+PY25 = PY2 and VERSION.minor == 5
+PY26 = PY2 and VERSION.minor == 6
+PY27 = PY2 and VERSION.minor == 7
 PY3 = not PY2
+PY31 = PY3 and VERSION.minor == 1
+PY32 = PY3 and VERSION.minor == 2
+PY33 = PY3 and VERSION.minor == 3
+py34 = PY3 and VERSION.minor == 4
 
 # Provide a nice range function for py2.
 try:
